9bf486b4926a9a6c03a924e9a016ca86fb4e0595
Mantz <orm109@gmail.com>
2015-06-10 19:02:22 +0300

    connet mq

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ var fs = require('fs');
 var path = require('path');
 var guid = require('guid');
 var Ebay = require('ebay');
-//var context = require('rabbit.js').createContext('amqp://10.0.0.9');
-var context = require('rabbit.js').createContext('amqp://yncidqyc:JH4x2YLUR_vyn4Y1CP2P6GyCHlvi96r8@owl.rmq.cloudamqp.com/yncidqyc');
+var context = require('rabbit.js').createContext('amqp://localhost');
+//var context = require('rabbit.js').createContext('amqp://yncidqyc:JH4x2YLUR_vyn4Y1CP2P6GyCHlvi96r8@owl.rmq.cloudamqp.com/yncidqyc');
 
 var push = context.socket('PUSH');
+var pushImg = context.socket('PUSH');
+var pull = context.socket('PULL');
+
 var GoogleSearch = require('google-search');
 var googleSearch = new GoogleSearch({
     key: 'AIzaSyAwFxWoXwWNts6fyZpN3cowCb5BXoL0qT4',
@@ -80,12 +83,13 @@ app.post('/api/items/google', function(req, res) {
     var imgUrl = req.body.imgUrl;
     var price = req.body.Price;
     var guid = req.body.Guid;
-    console.log('text:' + searchtext + ' price:' + price + 'imgUrl: ' + imgUrl + ' Guid: ' + guid);
+    //console.log('text:' + searchtext + ' price:' + price + 'imgUrl: ' + imgUrl + ' Guid: ' + guid);
 
 
     var numOfPages = 4;
     //var allUrls = [];
     //var count = 0;
+
     for (var i = 0; i < numOfPages; i++) {
         googleSearch1(searchtext, i * 10, function(data) {
             //console.log(data);
@@ -93,7 +97,7 @@ app.post('/api/items/google', function(req, res) {
             res.header('Content-type', 'application/json');
             res.header('Charset', 'utf8');
 
-            push.connect('TEST1', function() {
+            push.connect('TEST4', function() {
                 data.items.forEach(function(item) {
                     var obj = {};
                     obj.Url = item.link;
@@ -109,22 +113,37 @@ app.post('/api/items/google', function(req, res) {
     //goot image search 
 
     GoogleImageSearch.GetAllUrls(imgUrl, null, function(data) {
-        console.log('GetAllUrls');
-        console.log(data);
-        push.connect('TESTIMG2', function() {
+        // console.log('GetAllUrls');
+        //console.log(data);
+
+        pushImg.connect('TEST1', function() {
 
             data.forEach(function(item) {
                 var obj = {};
                 obj.Url = item;
                 obj.Price = price;
                 obj.Token = guid;
-                push.write(JSON.stringify(obj));
+                pushImg.write(JSON.stringify(obj));
             });
 
         });
     });
 });
 
+function pullMessages() {
+    console.log('in the pullMessages');
+    pull.connect('FinalOUT');
+
+    pull.on('data', function T(data) {
+        var inMsg = JSON.parse(data);
+        console.log('@@@@@@@@@@@@@@@@@@@@@');
+        console.log(inMsg.user);
+        //console.log(JSON.stringify(data));
+    });
+
+}
+
+pullMessages();
 
 
 function googleSearch22(searchtext, cb) {
@@ -141,13 +160,13 @@ function googleSearch22(searchtext, cb) {
             num: 10,
             fields: "items/link"
         }, function(error, response) {
-            console.log(response.items);
+            // console.log(response.items);
 
 
             count++;
             response.items.forEach(function(entity) {
                 allUrls.push(entity.link);
-                console.log('count=' + count + ' - ' + allUrls.length + '# - ' + entity.link);
+                //  console.log('count=' + count + ' - ' + allUrls.length + '# - ' + entity.link);
             });
 
             if (count == numOfPages) {
